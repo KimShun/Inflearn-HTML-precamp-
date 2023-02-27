5ee894a531cb9f0cd542961619deb0f0a847379a
김시현 <sihyun@gimsihyeon-ui-MacBookAir.local>
2023-02-27 19:40:12 +0900

    02-27

---
+++ PRECAMP/class/cyworld/script/index.js
@@ -0,0 +1,7 @@
+function changing__game() {
+    document.getElementById("iframe_content").src = "./game.html"
+}
+
+function changing__home() {
+    document.getElementById("iframe_content").src = "./home.html"
+}
